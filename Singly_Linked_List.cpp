ab9d15477d7e35d1b7b7cfb434a1182565bdb21a
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-09-04 23:12:27 +0530

    Update Singly_Linked_List.cpp

--- Singly_Linked_List.cpp
+++ Singly_Linked_List.cpp
@@ -71,3 +71,57 @@ void LinkedList()
         current = first;
     }
 }*/
+
+
+
+//Model 2
+/*#include <iostream>
+using namespace std;
+struct n
+{
+    int data;
+    struct n *ptr;
+};
+typedef struct n node;
+
+
+void LinkedList();
+int main()
+{
+    LinkedList();
+    return 0;
+}
+
+
+void LinkedList()
+{
+    node *first,*current,*list;
+    char c;
+    first = nullptr;
+    do
+    {
+        list = new node;
+        cout<<"Enter here: ";
+        cin>>list->data;
+        list->ptr = nullptr;
+        if(first == nullptr)
+         first=current=list;
+        else
+        {
+            list->ptr = first;
+            first->ptr = nullptr;
+            first = list;
+        }
+        cout<<"Do you want insert again (y/n): ";
+        cin>>c;
+    }while(c=='Y'||c=='y');
+    
+    
+    current = first;
+    while(current != nullptr)
+    {
+        first = first->ptr;
+        delete current;
+        current = first;
+    }
+}*/
