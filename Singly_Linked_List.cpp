4e13ee2ff819c1d2d8fa1445c4c479b24ccc909a
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-09-05 15:23:38 +0530

    Update Singly_Linked_List.cpp

--- Singly_Linked_List.cpp
+++ Singly_Linked_List.cpp
@@ -1,29 +1,31 @@
-//Under Research
-
-/*
 #include <iostream>
 using namespace std;
+
+
+//Structure for Linked_List -Singly
 struct n
 {
-    int data;
+    int data;         //Variables: data-(to store values) , ptr-(pointer to point address) 
     struct n *ptr;
 };
 typedef struct n node;
 
 
-void LinkedList();
-int main()
+//Class 'Call' to perform Linked List operation
+class Call
 {
-    LinkedList();
-    return 0;
-}
+    node *first,*current,*list;        //Variables:  first(Points to first node) , current(Points to node has NULL in link part) , list(For the creation of nodes)
+    public:
+    void accept();
+    void display();
+    ~Call();
+};
 
 
-//Linked list program
-void LinkedList()
+//Accept function
+void Call::accept()
 {
-    node *list,*first,*current;
-    char c;
+    char choice;
     first = nullptr;
     do
     {
@@ -31,97 +33,51 @@ void LinkedList()
         cout<<"Enter the value: ";
         cin>>list->data;
         list->ptr = nullptr;
+        list->ptr = nullptr;
         if(first == nullptr)
-         current = first = list;
+         first = current = list;
         else
         {
-            current->ptr = list;
-            current = list;
+          curent->ptr = list;
+          current = list;
         }
-        cout<<"Do you want to input again(y/n): ";
-        cin>>c;
-    }while(c == 'y' || c == 'Y');
-    
-    //Insertion at beginning
-    do
-    {
-        list = new node;
-        cout<<"Enter the data to insert: ";
-        cin>>list->data;
-        list->ptr = first;
-        first = list;
-        cout<<"Do you want to insert again: ";
-        cin>>c;
-    }while(c == 'Y' || c == 'y');
-    
-    //Display
-    current = first;
-    while(current != nullptr)
-    {
-        cout<<current->data<<endl;
-        current = current->ptr;
-    }
-    
-    //Deletion
-    current = first;
-    while(current != nullptr)
-    {
-        first = first->ptr;
-        delete current;
-        current = first;
-    }
-}*/
-
-
-
-//Model 2
-/*#include <iostream>
-using namespace std;
-struct n
-{
-    int data;
-    struct n *ptr;
-};
-typedef struct n node;
-
-
-void LinkedList();
-int main()
-{
-    LinkedList();
-    return 0;
+        cout<<"Do you want to enter the value again(y/n): ";
+        cin>>choice;
+     }while(choice == 'Y' || choice = 'y');
 }
 
 
-void LinkedList()
+//Display function
+void Call::display()
 {
-    node *first,*current,*list;
-    char c;
-    first = nullptr;
-    do
-    {
-        list = new node;
-        cout<<"Enter here: ";
-        cin>>list->data;
-        list->ptr = nullptr;
-        if(first == nullptr)
-         first=current=list;
-        else
-        {
-            list->ptr = first;
-            first->ptr = nullptr;
-            first = list;
-        }
-        cout<<"Do you want insert again (y/n): ";
-        cin>>c;
-    }while(c=='Y'||c=='y');
-    
-    
-    current = first;
+     current = start;
+     cout<<"Elements in linked list: \n";
+     while(current != nullptr)
+     {
+         cout<<current->data<<endl;
+         current = current->ptr;
+     }
+}
+
+
+//Deallocation function
+~Call::Call()
+{
+    current = start;
     while(current != nullptr)
     {
         first = first->ptr;
         delete current;
         current = first;
     }
-}*/
+}
+
+
+//Main function to execute
+int main()
+{
+    Call obj;
+    obj.accept();
+    obj.display();
+    return 0;
+}
