7671b51b67753ccd2506df176123505aa926134d
Alwin George <150264978+Alwin444@users.noreply.github.com>
2024-09-07 12:51:03 +0530

    Update SL.Concatenate_model.cpp

--- SL.Concatenate_model.cpp
+++ SL.Concatenate_model.cpp
@@ -1,1 +1,106 @@
+// Program Name: Concatenation of string using Linked List
+// Status: Model,Unstable
 
+#include <iostream>
+using namespace std;
+
+
+//For Linked list structures -  node & node2
+struct n
+{
+ char data;
+ struct n *ptr;
+};
+typedef struct n node;
+struct n2
+{
+ char data2;
+ struct n2 *ptr2;
+};
+typedef struct n2 node2;
+
+
+//Main Function starts
+int main()
+{
+ char choice;
+ node *first,*current,*list;
+ node *first2,*current2,*list2;
+ first = first2 = nullptr;
+
+ //Accepts character for first string
+ do
+ {
+  list = new node;
+  cout<<"Enter the character: ";
+  cin>>list->data;
+  list->ptr = nullptr;
+  if(first = nullptr)
+   first = current = list;
+  else
+  {
+   current->ptr = list;
+   current = list;
+  }
+  cout<<"Do you want to enter again: ";
+  cin>>choice;
+ }while(choice == 'y' || choice == 'Y');
+
+ cout<<"\nFor second string\n";
+
+ //Accepts character for second string
+ do
+ {
+  list2 = new node2;
+  cout<<"Enter the character: ";
+  cin>>list2->data2;
+  list2->ptr2 = nullptr;
+  if(first2 = nullptr)
+   first2 = current2 = list2;
+  else
+  {
+   current2->ptr2 = list2;
+   current2 = list2;
+  }
+  cout<<"Do you want to enter again: ";
+  cin>>choice;
+ }while(choice == 'y' || choice == 'Y');
+
+ current2 = first2;
+
+ //To attach Linked lists node & node2 for concatenation
+ while(current2 != nullptr)
+ {
+  current->ptr = current2;
+  current = current2;
+  current2 = current2->ptr2;
+ }
+
+ current = first;
+
+ //To diplay concatenated string(Without space)
+ while(current != nullptr)
+ {
+  cout<<current->data<<endl;
+  current = current->ptr;
+ }
+
+ current = first;
+ current2 = first2;
+
+ //To Deallocate both linked list node & node2
+ while(current != nullptr)
+ {
+  first = first->ptr;
+  delete current;
+  current = first;
+ }
+ while(current2 != nullptr)
+ {
+  first2 = first2->ptr2;
+  delete current2;
+  current2 = first2;
+ }
+ return 0;
+}
+ 
